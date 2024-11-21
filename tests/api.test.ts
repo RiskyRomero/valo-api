923adc91d648f0acd5e90a4daa5342e9db01dff6
Ryumin <RiskyRomero@proton.me>
2024-11-21 21:19:31 +0800

    chore: bump version to 0.1.3

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,4 +1,4 @@
-import VALOAPI from "../src/index";
+import VALOAPI, { IAccountDetails } from "../src/index";
 import { assert } from "chai";
 
 require('dotenv').config()
@@ -6,24 +6,23 @@ require('dotenv').config()
 runTests();
 
 function runTests() {
-    if (!process.env.TOKEN) 
-    {
-        return console.error("Token either not provided, is undefined, or null");
-    }
+  if (!process.env.TOKEN) {
+    return console.error("Token either not provided, is undefined, or null");
+  }
 
-    const valApi = new VALOAPI(process.env.TOKEN);
+  const valApi = new VALOAPI(process.env.TOKEN);
 
-    describe("Get Account Details", async () => {
-        it("Should return the account details successfully.", async () => {
-            const result = await valApi.getAccount({ name: "Ryumin", tag: "ELI" });
-            assert.equal(result.status, 200);
-        });
-
-        it("Should return a 404 upon searching for an invalid user.", async () => {
-            const res = await valApi.getAccount({ name: "ryumin3213", tag: "37213" });
-            assert.equal(res.status, 404);
-        })
+  describe("Get Account Details", async () => {
+    it("Should return the account details successfully.", async () => {
+      const result = await valApi.getAccount({ name: "Ryumin", tag: "ELI" });
+      assert.equal(result.status, 200);
     });
 
-    // More tests to be added, womp womp
+    it("Should return a 404 upon searching for an invalid user.", async () => {
+      const res = await valApi.getAccount({ name: "ryumin3213", tag: "37213" });
+      assert.equal(res.status, 404);
+    })
+  });
+
+  // More tests to be added, womp womp
 }
